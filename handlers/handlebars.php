06d6c16c262acbadb7c3836ea9e191ec83aa535d
jbroadway <johnny@johnnybroadway.com>
2012-06-27 00:20:10 -0500

    Updated assetic/handlebars handler

--- handlers/handlebars.php
+++ handlers/handlebars.php
@@ -1,7 +1,9 @@
 <?php
 
 /**
- * Precompiles handlebars templates for client-side rendering.
+ * Precompiles handlebars templates for client-side rendering. Avoids
+ * unnecessary client-side template compilation, and includes the much
+ * smaller Handlebars runtime without the compiler, so it's much faster.
  *
  * Usage:
  *
@@ -15,8 +17,9 @@
  * 3. After the above include has compiled the templates, you can include
  * them via:
  *
- *     <script src="/apps/assetic/js/handlebars.runtime.1.0.0.beta.6.js"></script>
- *     <script src="/cache/handlebars/all.js"></script>
+ *     <script src="/cache/assetic/handlebars.compiled.js"></script>
+ *
+ * > Note: This also includes the slimmer Handlebars runtime for you.
  *
  * 4. To call a template, simply refer to it like this:
  *
@@ -28,12 +31,16 @@
 // path to the handlebars compiler
 $handlebars = $appconf['Assetic']['handlebars'];
 
-// file to save cache
-$cache_dir = 'cache/handlebars';
+// path to save cache
+$cache_dir = $appconf['Assetic']['save_path'] . '/handlebars';
+if (! file_exists ($cache_dir)) {
+	mkdir ($cache_dir);
+	chmod ($cache_dir, 0777);
+}
 
 $files = glob ('apps/*/views/*.handlebars');
 
-$all = 'cache/handlebars/all.js';
+$all = $appconf['Assetic']['save_path'] . '/handlebars.compiled.js';
 if (! file_exists ($all)) {
 	$recompile = true;
 } else {
@@ -50,7 +57,7 @@ foreach ($files as $file) {
 }
 
 if ($recompile) {
-	file_put_contents ($all, '');
+	file_put_contents ($all, file_get_contents ('apps/assetic/js/handlebars.runtime-1.0.0.beta.6.js');
 	foreach ($files as $file) {
 		$cache_file = $cache_dir . '/' . basename ($file, '.handlebars') . '.js';
 		file_put_contents ($all, file_get_contents ($cache_file) . ";\n", FILE_APPEND);
